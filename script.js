5ed56c69ce944c2a899ec5ed4381f4fbd13a7862
Alexisrdz1219 <ealexisrdz19@gmail.com>
2026-02-03 21:14:55 -0600

    agregue script ya correcto

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("🔥 script.js cargado correctamente");
+
 fetch("https://buscador-refaccionesbackend.onrender.com/health")
   .then(res => res.json())
   .then(data => {
@@ -5,4 +7,24 @@ fetch("https://buscador-refaccionesbackend.onrender.com/health")
   })
   .catch(err => {
     console.error("ERROR:", err);
+  });
+
+  const statusDiv = document.getElementById("backend-status");
+
+fetch("https://buscador-refaccionesbackend.onrender.com/health")
+  .then(res => res.json())
+  .then(data => {
+    if (data.ok) {
+      statusDiv.innerHTML = `
+        <p style="color:green;">✅ Backend conectado</p>
+        <p style="color:green;">✅ Base de datos conectada</p>
+        <p>🕒 Hora servidor: ${data.time}</p>
+      `;
+    } else {
+      statusDiv.innerHTML = "⚠️ Backend respondió, pero algo falló";
+    }
+  })
+  .catch(err => {
+    statusDiv.innerHTML = "❌ No se pudo conectar al backend";
+    console.error(err);
   });
